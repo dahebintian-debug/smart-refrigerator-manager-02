1f1c745ec2b3466faf24ac292aa8481d93cf484f
YamatoHamada <251253452+dahebintian-debug@users.noreply.github.com>
2025-12-27 10:30:34 +0900

    feat: login pre-stable

--- src/main/java/com/example/springphoto/controller/FoodController.java
+++ src/main/java/com/example/springphoto/controller/FoodController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import jakarta.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +24,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*") 
 public class FoodController {
+	@Autowired
     private final FoodService foodService;
+    
 
     @GetMapping
     public List<Food> getFoods() {
